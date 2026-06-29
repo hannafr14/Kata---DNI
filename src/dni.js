c81dae576c3c5b94dc8ac8f31a3174bf132e9785
Hanna <hannafrolova14@gmail.com>
2026-06-29 19:49:28 +0200

    feat: add negative number validation

--- src/dni.js
+++ src/dni.js
@@ -2,6 +2,11 @@ const DNI_LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
 
 export function calculateDniLetter(value) {
   const num = Number(value);
+
+  if(num < 0) {
+    return null;
+  }
+  
   const remainder = num % 23;
 
   return DNI_LETTERS[remainder];
