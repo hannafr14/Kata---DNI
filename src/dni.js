9e06c5d669b4d22658695f1e1c9f50db99aaf98e
Hanna <hannafrolova14@gmail.com>
2026-06-29 19:58:23 +0200

    feat: add upper range validation

--- src/dni.js
+++ src/dni.js
@@ -3,10 +3,10 @@ const DNI_LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
 export function calculateDniLetter(value) {
   const num = Number(value);
 
-  if(num < 0) {
+  if(num < 0 || num > 99999999) {
     return null;
   }
-  
+
   const remainder = num % 23;
 
   return DNI_LETTERS[remainder];
