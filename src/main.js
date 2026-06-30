59490b7aa0fdab3b5a03bea2f4bc25b9cf74084c
Hanna <hannafrolova14@gmail.com>
2026-06-30 09:11:54 +0200

    feat: add cancel process navigation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 const introSection = document.querySelector("#intro-section");
 const calculatorSection = document.querySelector("#calculator-section");
+const finishedSection = document.querySelector("#finished-section");
+
 const startButton = document.querySelector("#start-button");
+const cancelButton = document.querySelector("#cancel-button");
 
 startButton.addEventListener("click", () => {
     introSection.hidden = true;
     calculatorSection.hidden = false;
+});
+
+cancelButton.addEventListener("click", () => {
+  calculatorSection.hidden = true;
+  finishedSection.hidden = false;
 });
